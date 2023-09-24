b950f75a219c6e6b0960235ee4f82ad7dbdb40e6
BigCookie233 <86756026+BigCookie233@users.noreply.github.com>
2023-09-24 20:40:55 +0800

    Update onCommand.java

--- VelocityTeleporter-Spigot/src/main/java/io/github/bigcookie233/velocityteleporter/onCommand.java
+++ VelocityTeleporter-Spigot/src/main/java/io/github/bigcookie233/velocityteleporter/onCommand.java
@@ -31,19 +31,4 @@ public class onCommand implements CommandExecutor, TabCompleter {
     public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
         return new ArrayList<>();
     }
-
-    private List<String> getMatchedStrings(String string, String[] strings) {
-        ArrayList<String> ret = new ArrayList<>();
-        for (String s: strings) {
-            if (compareStringIgnoreCase(s, string)) {
-                ret.add(s);
-            }
-        }
-        return ret;
-    }
-
-    private boolean compareStringIgnoreCase(String s1, String s2) {
-        if (s2.length() > s1.length()) return false;
-        return s1.substring(0, s2.length()).equalsIgnoreCase(s2);
-    }
 }
