2a330049fa92b71f9bba95dca686c7fcd898094e
BigCookie233 <86756026+BigCookie233@users.noreply.github.com>
2024-02-25 14:45:01 +0800

    feat: Add a parameter to /velocityteleporter command

--- VelocityTeleporter-Spigot/src/main/java/io/github/bigcookie233/velocityteleporter/onCommand.java
+++ VelocityTeleporter-Spigot/src/main/java/io/github/bigcookie233/velocityteleporter/onCommand.java
@@ -13,18 +13,23 @@ import java.util.List;
 public class onCommand implements CommandExecutor, TabCompleter {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if (!(commandSender instanceof Player)) {
-            commandSender.sendMessage(ChatColor.RED + "You must be a player to run this command.");
-            return false;
+        if (strings.length == 1) {
+            if (!(commandSender instanceof Player)) {
+                commandSender.sendMessage(ChatColor.RED + "You must be a player to run this command.");
+                return false;
+            }
+            VelocityTeleporter.getInstance().sendMsg((Player) commandSender, strings[0]);
+            return true;
+        } else if (strings.length == 2) {
+            Player player = VelocityTeleporter.getInstance().getServer().getPlayer(strings[1]);
+            if (player == null) {
+                commandSender.sendMessage(ChatColor.RED + "Player not found.");
+                return false;
+            }
+            VelocityTeleporter.getInstance().sendMsg(player, strings[0]);
+            return true;
         }
-
-        if (strings.length != 1) {
-            return false;
-        }
-
-        VelocityTeleporter.getInstance().sendMsg((Player) commandSender, strings[0]);
-
-        return true;
+        return false;
     }
 
     @Override
